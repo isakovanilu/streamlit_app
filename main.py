7f34d65409a0bd85615a6a152af9e88c5a1ac5da
Nilu Isakova <niluisakova@gmail.com>
2023-11-13 11:10:44 -0600

    add creds

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import streamlit as st
 from controller import StreamlitController
 from view import set_page_config, display_sidebar, display_data_frame, add_header
 
+SNOWFLAKE_CREDENTIALS = {
+    # add your Snowflake credentials here
+}
 
+def main():
+    set_page_config()
+
+    controller = StreamlitController(SNOWFLAKE_CREDENTIALS)
+    add_header("Welcome to the Streamlit App")
