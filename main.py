088458a78dd10a0e7274217210aa6fee6b91dd11
Nilu Isakova <niluisakova@gmail.com>
2023-11-15 09:50:53 -0600

    query template

--- main.py
+++ main.py
@@ -17,4 +17,17 @@ def main():
     sql_query = """SELECT COL1, COL2 FROM TABLE1; """
     data = controller.get_data(sql_query)
 
+    if data is not None:
+        # display the sidebar and get the selected columns
+        selected_columns = display_sidebar(data)
+        
+        # filter data based on the sidebar selection
+        filtered_data = controller.filter_data(data, 'COL1', selected_columns)
+        
+        # display the filtered data
+        display_data_frame(filtered_data)
+    else:
+        st.error("Failed to load data.")
 
+if __name__ == "__main__":
+    main()
