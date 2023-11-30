956ac7f4f1ac55fafb33d2b465647ba2c27d0165
Nilu Isakova <niluisakova@gmail.com>
2023-11-29 21:27:01 -0600

    added docstrngs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
-import streamlit as st
 import os
+import streamlit as st
 from controller import StreamlitController
 from view import set_page_config, display_sidebar, display_data_frame, add_header
 
@@ -10,24 +9,29 @@ SNOWFLAKE_CREDENTIALS = {
     "account": os.getenv("SNOWFLAKE_ACCOUNT")
 }
 
+
 def main() -> None:
+    """This fucntion runs the streamlit app"""
     set_page_config()
 
     controller = StreamlitController(SNOWFLAKE_CREDENTIALS)
     add_header("Welcome to the Streamlit App")
 
     try:
-        sql_query = st.text_area("Enter your SQL query", """SELECT COL1, COL2 FROM TABLE1; """)
+        sql_query = st.text_area(
+            "Enter your SQL query", """SELECT COL1, COL2 FROM TABLE1; """)
         data = controller.get_data(sql_query)
 
         if data is not None:
             selected_columns = display_sidebar(data)
-            filtered_data = controller.filter_data(data, 'COL1', selected_columns)
+            filtered_data = controller.filter_data(
+                data, 'COL1', selected_columns)
             display_data_frame(filtered_data)
         else:
             st.error("No data returned from the query.")
     except Exception as e:
         st.error(f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     main()
