48774f455c450dbd99cb7f9a0644160b53ee1227
Nilu Isakova <niluisakova@gmail.com>
2023-12-04 23:45:09 -0600

    added docstrngs

--- controller.py
+++ controller.py
@@ -15,6 +15,7 @@ class StreamlitController:
 
     def filter_data(self, data: pd.DataFrame, column: str, values: List[str] = None, 
                     range_data: tuple = None, inclusive: bool = True) -> pd.DataFrame:
+        """This function filters the data based on the input selections"""
         if column not in data.columns:
             # Log warning or raise an error
             return data
