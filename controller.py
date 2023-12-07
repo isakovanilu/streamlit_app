06315ba2373937891400b4e17d12a8fdbe5ea84b
Nilu Isakova <niluisakova@gmail.com>
2023-12-06 22:03:48 -0600

    added docstrngs, format

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ from model import SnowflakeModel
 import pandas as pd
 from typing import List, Any
 
+
 class StreamlitController:
     def __init__(self, credentials: Any) -> None:
         self.model = SnowflakeModel(credentials)
@@ -14,13 +15,13 @@ class StreamlitController:
             # Log the exception (or handle it as required)
             raise e
 
-    def filter_data(self, data: pd.DataFrame, column: str, values: List[str] = None, 
+    def filter_data(self, data: pd.DataFrame, column: str, values: List[str] = None,
                     range_data: tuple = None, inclusive: bool = True) -> pd.DataFrame:
         """This function filters the data based on the input selections"""
         if column not in data.columns:
             # Log warning or raise an error
             return data
-        
+
         if range_data:
             return data[data[column].between(*range_data, inclusive='both' if inclusive else 'neither')]
         elif values:
