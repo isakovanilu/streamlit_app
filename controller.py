95c88c0ca7fc5e76dbb8208768da722423c9d7af
Nilu Isakova <niluisakova@gmail.com>
2023-12-11 21:12:30 -0600

    clean up

--- controller.py
+++ controller.py
@@ -1,9 +1,11 @@
-from model import SnowflakeModel
-import pandas as pd
 from typing import List, Any
+import pandas as pd
+from model import SnowflakeModel
 
 
 class StreamlitController:
+    """This is controler for streamlit"""
+
     def __init__(self, credentials: Any) -> None:
         self.model = SnowflakeModel(credentials)
 
@@ -15,16 +17,21 @@ class StreamlitController:
             # Log the exception (or handle it as required)
             raise e
 
-    def filter_data(self, data: pd.DataFrame, column: str, values: List[str] = None,
-                    range_data: tuple = None, inclusive: bool = True) -> pd.DataFrame:
+    def filter_data(
+            self,
+            data: pd.DataFrame,
+            column: str,
+            values: List[str] = None,
+            range_data: tuple = None,
+            inclusive: bool = True) -> pd.DataFrame:
         """This function filters the data based on the input selections"""
         if column not in data.columns:
             # Log warning or raise an error
             return data
 
         if range_data:
-            return data[data[column].between(*range_data, inclusive='both' if inclusive else 'neither')]
-        elif values:
+            return data[data[column].between(
+                *range_data, inclusive='both' if inclusive else 'neither')]
+        if values:
             return data[data[column].isin(values)]
-        else:
-            return data
+        return data
